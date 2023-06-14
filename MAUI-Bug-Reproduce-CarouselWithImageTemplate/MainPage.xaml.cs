2403996c1b1b850e4ed8a1978cfd0b7e7aa2ef92
DosangGu-Andy <ddos0418@gmail.com>
2023-06-14 16:54:31 +0900

    provisionging

--- MAUI-Bug-Reproduce-CarouselWithImageTemplate/MainPage.xaml.cs
+++ MAUI-Bug-Reproduce-CarouselWithImageTemplate/MainPage.xaml.cs
@@ -4,21 +4,11 @@ public partial class MainPage : ContentPage
 {
 	int count = 0;
 
+	private IReadOnlyList<string> _images = new List<string>() { "dotnet_bot", "dotnet_bot", "dotnet_bot"};
+
 	public MainPage()
 	{
 		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }
 
---
+++ MAUI-Bug-Reproduce-CarouselWithImageTemplate/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MAUI_Bug_Reproduce_CarouselWithImageTemplate;
+
+internal class MainViewModel
+{
+    public IReadOnlyList<string> Images { get; } = new List<string>() { "dotnet_bot", "dotnet_bot", "dotnet_bot" };
+
+    public string ImageOne { get; } = "dotnet_bot";
+
+    public MainViewModel()
+    {
+
+    }
+}
